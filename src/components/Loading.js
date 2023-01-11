4368585734da840b83f3f5b88e5a5a9c204c9bca
abbas ejlali <abbas.ejlali18@gmail.com>
2023-01-11 07:33:10 -0800

    add gif to loading.js

---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// gif
+import loadinggif from "../gif/Double Ring-2.3s-151px.gif";
+
+const Loading = () => {
+  return (
+    <div>
+      <img src={loadinggif} alt="loading gif" />
+    </div>
+  );
+};
+
+export default Loading;
